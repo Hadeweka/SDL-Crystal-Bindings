a0731736178240b1e3fafc2347c63fad9cc69bfb
Philipp Schulz <hadeweka@outlook.de>
2025-01-02 01:16:38 +0100

    Finished first example

--- sdl_examples/01-clear/main.cr
+++ sdl_examples/01-clear/main.cr
@@ -2,9 +2,11 @@
 
 require "../../src/sdl-crystal-bindings.cr"
 
+# NOTE: We don't have static variables in Crystal, but class properties will do
 class Globals
   class_property window = Pointer(LibSDL::Window).null
   class_property renderer = Pointer(LibSDL::Renderer).null
+  class_property initial_time = Time.utc
 end
 
 def app_init_func(appdata : Void**, argc : LibC::Int, argv : LibC::Char**)
@@ -27,13 +29,18 @@ def app_init_func(appdata : Void**, argc : LibC::Int, argv : LibC::Char**)
 end
 
 def app_iterate_func(appdata : Void*)
-  # TODO: Add timer
-  red = 0.5
-  green = 0.5
-  blue = 0.5
+  # NOTE: The time and timer functions from SDL are not included, as Crystal already provides a good API for these
+  now = (Time.utc - Globals.initial_time).total_milliseconds / 1000.0
+
+  # NOTE: SDL's math modules are also not included, for the same reason
+  red = 0.5 + 0.5 * Math.sin(now)
+  green = 0.5 + 0.5 * Math.sin(now + Math::PI * 2 / 3)
+  blue = 0.5 + 0.5 * Math.sin(now + Math::PI * 4 / 3)
   LibSDL.set_render_draw_color_float(Globals.renderer, red, green, blue, LibSDL::ALPHA_OPAQUE_FLOAT)
+
   LibSDL.render_clear(Globals.renderer)
   LibSDL.render_present(Globals.renderer)
+
   return LibSDL::AppResult::APP_CONTINUE
 end
 
@@ -49,4 +56,9 @@ def app_quit_func(appstate : Void*, result : LibSDL::AppResult)
 end
 
 # NOTE: This is not a very elegant way to enable these callbacks, but it works for now
-LibSDL.enter_app_main_callbacks(0, nil, ->app_init_func, ->app_iterate_func, ->app_event_func, ->app_quit_func)
+# NOTE: This might change at a later point
+def sdl_main(argc : Int32, argv : UInt8**)
+  LibSDL.enter_app_main_callbacks(argc, argv, ->app_init_func, ->app_iterate_func, ->app_event_func, ->app_quit_func)
+end
+
+LibSDL.run_app(ARGC_UNSAFE, ARGV_UNSAFE, ->sdl_main, nil)
